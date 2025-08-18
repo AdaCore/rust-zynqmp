d74e1351bcfac798f49ec1516661d64ea16a6b44
Tobias Reiher <reiher@adacore.com>
2025-08-18 11:32:08 +0200

    Add testing of examples
    Issue: eng/toolchain/rust/zynqmp-bsp#3

---
+++ cortex-a-rt/examples/floating_point.rs
@@ -0,0 +1,21 @@
+#![no_std]
+#![no_main]
+
+use qemu_exit::QEMUExit;
+
+extern crate cortex_a_rt;
+
+cortex_a_rt::entry!(main);
+
+fn main() -> ! {
+    let mut x = 1.0;
+    x += 2.0;
+    assert_eq!(x, 3.0);
+
+    qemu_exit::AArch64::new().exit_success();
+}
+
+#[panic_handler]
+fn panic(_panic: &core::panic::PanicInfo<'_>) -> ! {
+    qemu_exit::AArch64::new().exit_failure();
+}
